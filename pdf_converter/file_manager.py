4d584689f85d458b421e6d02bdb3cba72845742e
houru <houruxuan01@gmail.com>
2025-05-17 20:42:46 +1000

    (feat) initial files for file converter class and non-working UI template

---
+++ pdf_converter/file_manager.py
@@ -0,0 +1,63 @@
+from PIL import Image
+from enum import StrEnum
+from pathlib import Path
+
+
+class ConversionType(StrEnum):
+    PNG = 'PNG'
+    JPG = 'JPG'
+    PDF = 'PDF'
+    COMBINED_PDF = 'COMBINED PDF'
+
+
+CONVERSION_TYPE_TO_FILE_FORMAT: dict[ConversionType, str] = {
+    ConversionType.PNG: '.png',
+    ConversionType.JPG: '.jpg',
+    ConversionType.PDF: '.pdf',
+    ConversionType.COMBINED_PDF: '.pdf',
+}
+
+
+class FileManager:
+
+    def __init__(self):
+        self.file_paths = []
+
+    def add_file_path(self, new_paths: list[str]) -> None:
+        for p in new_paths:
+            self.file_paths.append(Path(p))
+
+    def delete_file_path(self, indices: list[int]) -> None:
+        for i in sorted(indices, reverse=True):
+            del self.file_paths[i]
+
+    def clear_file_paths(self) -> None:
+        self.file_paths = []
+
+    def switch_order(self, i: int, j: int) -> None:
+        self.file_paths[i], self.file_paths[j] = self.file_paths[j], self.file_paths[i]
+
+    def display_files(self) -> list[str]:
+        return [p.name for p in self.file_paths]
+
+    def convert_files(self, c_type: ConversionType, save_loc: Path) -> str:
+        try:
+            self._convert_files(c_type, save_loc)
+        except Exception as e:
+            return str(e)
+
+    def _convert_files(self, convertion_type_enum: ConversionType, save_location: Path) -> None:
+        fmt = CONVERSION_TYPE_TO_FILE_FORMAT[convertion_type_enum]
+        match convertion_type_enum:
+            case ConversionType.JPG | ConversionType.JPG | ConversionType.PDF:
+                for path in self.file_paths:
+                    im = Image.open(path)
+                    file_name = f"{save_location.parent}/{path.with_suffix(fmt).name}"
+                    im.save(file_name)
+            case ConversionType.COMBINED_PDF:
+                images = [Image.open(p) for p in self.file_paths]
+                # for combined pdfs, use parent folder as the file name
+                file_name = f"{save_location.parent}/{save_location.parent.with_suffix(fmt).name}"
+                images[0].save(file_name, save_all=True, append_images=images[1:])
+            case _:
+                raise ValueError(f"{convertion_type_enum} is not implemented")
